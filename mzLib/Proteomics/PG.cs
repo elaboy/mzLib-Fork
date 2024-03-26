c45a5145f4ffb7f56c1e2c285a56459bbb2ac372
Edwin Laboy <63374885+elaboy@users.noreply.github.com>
2024-03-26 17:37:37 -0500

    init

---
+++ mzLib/Proteomics/PG.cs
@@ -0,0 +1,44 @@
+﻿using Chemistry;
+using System.Collections.Generic;
+namespace Proteomics;
+public class PG
+{
+    public Nterminus Nt = new Nterminus();
+    public Cterminus Ct = new Cterminus();
+    public List<Node> Nodes { get; private set; }
+    public double SyspH { get; private set; }
+    public PG()
+    {
+        //always start with a node for the N-terminus
+
+        //always end with the C-terminus
+
+    }
+}
+
+public class PGExtensions
+{
+    public double GetDipoleMoment()
+    {
+        return 0;
+    }
+
+    public double GetMass()
+    {
+        return 0;
+    }
+
+    public double GetCharge()
+    {
+        return 0;
+    }
+}
+public static class Elements
+{
+    public static Element H = new Element("H", 1, 1.00782503207);
+    public static Element C = new Element("C", 6, 12.0000000000);
+    public static Element N = new Element("N", 7, 14.00307400443);
+    public static Element O = new Element("O", 8, 15.99491461957);
+    public static Element S = new Element("S", 16, 31.97207100);
+    public static Element P = new Element("P", 15, 30.97376199842);
+}
---
+++ mzLib/Proteomics/PGComponents.cs
@@ -0,0 +1,60 @@
+﻿using Chemistry;
+
+namespace Proteomics;
+public class Nterminus
+{
+    public static ChemicalFormula AtomicComposition = new ChemicalFormula(ChemicalFormula.ParseFormula("C2H3NO"));
+    public static ChemicalFormula? Modification = new ChemicalFormula();
+
+    public Nterminus()
+    {
+
+    }
+}
+
+public class Cterminus
+{
+    public static ChemicalFormula AtomicComposition = new ChemicalFormula(ChemicalFormula.ParseFormula("C2H3O2"));
+
+    public Cterminus()
+    {
+
+    }
+}
+
+public class Node
+{
+    public Backbone NodeBackbone { get; private set; }
+    public RGroup NodeResidue { get; private set; }
+    public Node? PreviousNode { get; private set; }
+    public Node? NextNode { get; private set; }
+    public int NodeNumber { get; private set; }
+
+    public Node(RGroup residue, Node? previousNode, Node? nextNode)
+    {
+        NodeResidue = residue;
+        PreviousNode = previousNode;
+        NextNode = nextNode;
+    }
+}
+
+public class RGroup
+{
+    public RGroup()
+    {
+
+    }
+}
+
+public class Backbone
+{
+
+    public Backbone()
+    {
+
+    }
+}
+
+
+
+
