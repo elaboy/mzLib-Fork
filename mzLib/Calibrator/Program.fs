41e76a2dd56d11bdf2bf41abc7dbf7aab79db8ba
Edwin Laboy <63374885+elaboy@users.noreply.github.com>
2024-09-18 06:59:06 -0500

    notebook

---
+++ mzLib/Calibrator/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+open Readers
+
+[<EntryPoint>]
+let main argv =
+    let path = 
+        @"D:\MannPeptideResults\A549_AllPeptides.psmtsv"
+    let file = 
+        Readers.PsmFromTsvFile path
+    file.LoadResults()
+
+    for psm in file.Results do
+        printfn "%s" psm.BaseSequence
+    0
