da2a29f12dae0ff6b134545352ce43ef7543289f
Edwin Laboy <63374885+elaboy@users.noreply.github.com>
2024-10-15 12:36:34 -0500

    test methods before proceeding

--- mzLib/RetentionTimeCalibration/RetentionTimeCalibration.cs
+++ mzLib/RetentionTimeCalibration/RetentionTimeCalibration.cs
@@ -1,27 +1,80 @@
 ﻿using MassSpectrometry;
+using Microsoft.ML;
+using Microsoft.ML.Data;
 using Readers;
+using System.Data;
+using Readers.QuantificationResults;
 
 namespace RetentionTimeCalibration;
 public class RetentionTimeCalibration
 {
-    public List<PsmFromTsvFile> ResultsFiles { get; set; }
+    public List<FileResultsMapper> ResultsFiles { get; set; }
+    public List<Species> CalibratedSpecies { get; set; }
     public List<Mzml> CalibratedFiles { get; set; }
 
-    private void GetModelPipeline()
+    public RetentionTimeCalibration(List<(
+        string peptidesResultsFilePath, 
+        string quantifiedPeakFilePath, 
+        string mzmlPath)> resultsFilesPath)
     {
-    }
+        FileResultsMapper[] resultsFiles = new FileResultsMapper[resultsFilesPath.Count];
 
+        Parallel.For(0, resultsFilesPath.Count, i =>
+        {
+            PsmFromTsvFile psmFromTsvFile = 
+                new PsmFromTsvFile(resultsFilesPath[i].peptidesResultsFilePath);
+            psmFromTsvFile.LoadResults();
+
+            Mzml mzmlFile = new Mzml(resultsFilesPath[i].mzmlPath);
+            mzmlFile.LoadAllStaticData();
+
+            resultsFiles[i] = new FileResultsMapper(psmFromTsvFile, mzmlFile);
+            FileResultsMapper mapper = new FileResultsMapper(psmFromTsvFile, mzmlFile);
+
+        });
+
+        ResultsFiles = resultsFiles.ToList();
+    }
     private void WriteCalibratedMzmls()
     {
+        throw new NotImplementedException();
     }
 
-    private void GetAnchors()
+    private List<Species> GetAnchors()
     {
+        var allSpecies = ResultsFiles.SelectMany(x => x.Species);
+        List<Species> anchorSpecies =
+            allSpecies.GroupBy(s => new { s.FullSequence, s.BaseSequence })
+                .Where(g => g.Count() == ResultsFiles.Count)
+                .Select(g => g.First())
+                .ToList();
+
+        return anchorSpecies;
     }
 
+    private List<Species> SpeciesToPredict(List<Species> anchors)
+    {
+        var allSpecies = ResultsFiles.SelectMany(x => x.Species);
+        List<Species> speciesToPredict = allSpecies.Except(anchors, new SpeciesComparer()).ToList();
+
+        return speciesToPredict;
+    }
 }
 
-internal class FileResultsMapper
+public class SpeciesComparer : IEqualityComparer<Species>
+{
+    public bool Equals(Species x, Species y)
+    {
+        return x.FullSequence == y.FullSequence && x.BaseSequence == y.BaseSequence;
+    }
+
+    public int GetHashCode(Species obj)
+    {
+        return HashCode.Combine(obj.FullSequence, obj.BaseSequence);
+    }
+}
+
+public class FileResultsMapper
 {
     public PsmFromTsvFile Results { get; set; }
     public Mzml MzmlFile { get; set; }
@@ -31,24 +84,93 @@ internal class FileResultsMapper
     {
         Results = psmFromTsvFile;
         MzmlFile = mzmlFile;
+        Species = GetSpecies();
     }
 
     public List<Species> GetSpecies()
     {
-        throw new NotImplementedException();
+        List<Species> species = new List<Species>();
+
+        foreach (var psm in Results.Results)
+        {
+            MsDataScan msDataScan = MzmlFile.GetOneBasedScan(psm.PrecursorScanNum);
+            Species newSpecies = new Species(
+                psm.FullSequence,
+                psm.BaseSeq,
+                psm.FileNameWithoutExtension,
+                psm.RetentionTime.Value, 
+                msDataScan);
+
+            species.Add(newSpecies);
+        }
+
+        return species;
     }
 }
 
-internal class Species
+
+public static class Aligner
+{
+    public static PredictionEngine<PreCalibrated, Calibrated> GetOLSPredictionEngine(List<Species> anchors)
+    {
+        MLContext mlContext = new MLContext();
+
+        List<PreCalibrated> preCalibratedAnchors = anchors.Select(x => new PreCalibrated()
+        {
+            AnchorRetentionTime = (float)x.ScanRetentionTime,
+            BaseSequence = x.BaseSequence,
+            FullSequence = x.FullSequence
+        }).ToList();
+
+
+        IDataView? dataView = mlContext.Data.LoadFromEnumerable(preCalibratedAnchors);
+
+        var pipeline = mlContext.Transforms.CopyColumns(
+                "Label",
+                nameof(PreCalibrated.AnchorRetentionTime))
+            .Append(mlContext.Transforms
+                .Concatenate("Features", nameof(PreCalibrated.UnCalibratedRetentionTime)))
+            .Append(mlContext.Regression.Trainers.Ols("Label", "Features"));
+
+        var model = pipeline.Fit(dataView);
+
+        return mlContext.Model.CreatePredictionEngine<PreCalibrated, Calibrated>(model);
+    }
+
+    //public static List<Species> PredictSpeciesRetentionTime(
+    //    PredictionEngine<PreCalibrated, Calibrated> predictionEngine, List<Species> speciesToPredictRT)
+    //{
+
+    //}
+}
+
+public class PreCalibrated
+{
+    [NoColumn]
+    public string FullSequence { get; set; }
+    [NoColumn]
+    public string BaseSequence { get; set; }
+
+    public float AnchorRetentionTime { get; set; }
+    public float UnCalibratedRetentionTime { get; set; }
+}
+
+public class Calibrated
+{
+    [ColumnName("Score")]
+    public float CalibratedRetentionTime { get; set; }
+}
+
+public class Species
 {
     public string FullSequence { get; set; }
     public string BaseSequence { get; set; }
     public string FileName { get; set; }
-    public double MsTwoRetentionTime { get; set; }
-    public double PeakIntensity { get; set; }
-    public double PeakRetentionTimeStart { get; set; }
-    public double PeakRetentionTimeApex { get; set; }
-    public double PeakRetentionTimeEnd { get; set; }
+    public double? MsTwoRetentionTime { get; set; }
+    public double? PeakIntensity { get; set; }
+    public double? PeakRetentionTimeStart { get; set; }
+    public double? PeakRetentionTimeApex { get; set; }
+    public double? PeakRetentionTimeEnd { get; set; }
     public double ScanRetentionTime { get; set; }
     public MsDataScan MsDataScan { get; set; }
 
@@ -69,13 +191,19 @@ internal class Species
         PeakRetentionTimeEnd = peakRetentionTimeEnd;
         ScanRetentionTime = scanRetentionTime;
     }
+    public Species(string fileName, string baseSequence,
+        string fullSequence, double scanRetentionTime, MsDataScan msDataScan)
+    {
+        FullSequence = fullSequence;
+        BaseSequence = baseSequence;
+        FileName = fileName;
+        ScanRetentionTime = scanRetentionTime;
+        MsDataScan = msDataScan;
+        ScanRetentionTime = scanRetentionTime;
+    }
 }
 
 internal class FileAligner
 {
 
-}
-
-internal class Aligner
-{
 }
---
+++ mzLib/Test/TestRetentionTimeCalibration.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Easy.Common.Extensions;
+using NUnit.Framework;
+
+namespace Test;
+
+[TestFixture]
+public class TestRetentionTimeCalibration
+{
+    [Test]
+    public void TestRetentionTimeCalibrationConstructor()
+    {
+        // Arrange
+        string peptidesResultsFilePath = @"E:\DatasetsForRT\Mann_11cell_lines\A549\2024-07-10-15-53-11_ClassigBigSearch\Task4-SearchTask\Individual File Results\20100604_Velos1_TaGe_SA_A549_1-calib-averaged_Peptides.psmtsv";
+        string quantifiedPeakFilePath = @"E:\\DatasetsForRT\\Mann_11cell_lines\\A549\\2024-07-10-15-53-11_ClassigBigSearch\\Task4-SearchTask\\Individual File Results\\20100604_Velos1_TaGe_SA_A549_1-calib-averaged_QuantifiedPeaks.tsv\";
+        string mzmlPath = @"E:\DatasetsForRT\Mann_11cell_lines\A549\2024-07-10-15-53-11_ClassigBigSearch\Task1-CalibrateTask\20100604_Velos1_TaGe_SA_A549_1-calib.mzML";
+        List<(string, string, string)> resultsFilesPath = new List<(string, string, string)> { (peptidesResultsFilePath, quantifiedPeakFilePath, mzmlPath) };
+
+        // Act
+        RetentionTimeCalibration.RetentionTimeCalibration retentionTimeCalibration = 
+            new RetentionTimeCalibration.RetentionTimeCalibration(resultsFilesPath);
+
+        // Assert
+        Assert.That(retentionTimeCalibration.ResultsFiles.Count == 1);
+        Assert.That(retentionTimeCalibration.ResultsFiles.First().Species.Count > 0);
+        Assert.That(retentionTimeCalibration.ResultsFiles.First().MzmlFile.IsNotNullOrEmpty());
+        Assert.That(retentionTimeCalibration.ResultsFiles.First().Results.Results.Count > 0);
+    }
+}
