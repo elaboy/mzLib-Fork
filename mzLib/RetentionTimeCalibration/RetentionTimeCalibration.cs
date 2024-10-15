9b96856fb590ae6587855354f377375857e0f6ce
Edwin Laboy <63374885+elaboy@users.noreply.github.com>
2024-10-14 20:29:25 -0500

    init

---
+++ mzLib/RetentionTimeCalibration/RetentionTimeCalibration.cs
@@ -0,0 +1,30 @@
+﻿using Readers;
+
+namespace RetentionTimeCalibration;
+public class RetentionTimeCalibration
+{
+    public List<PsmFromTsvFile> ResultsFiles { get; set; }
+    public List<PsmFromTsvFile> CalibratedFiles { get; set; }
+
+}
+
+internal class FileResultsMapper
+{
+    public PsmFromTsvFile Results { get; set; }
+    public Mzml MzmlFile { get; set; }
+
+    public FileResultsMapper(PsmFromTsvFile psmFromTsvFile, Mzml mzmlFile)
+    {
+        Results = psmFromTsvFile;
+        MzmlFile = mzmlFile;
+    }
+}
+
+internal class FileAligner
+{
+
+}
+
+internal class Aligner
+{
+}
